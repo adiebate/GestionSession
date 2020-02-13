e9c2dd688857a37b0929165c04d17db3ac7b1b0b
S2LF <sdeuhelf@gmail.com>
2020-02-13 11:13:04 +0100

    Syl20 - Ajout SessionForm

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Entity\Contenir;
+use App\Form\SessionFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -27,6 +29,33 @@ class SessionController extends AbstractController
             'sessions' => $sessions,
         ]);
     }
+
+    /**
+    * @Route("/new", name="form_session")
+    */
+    public function newForm(Request $request){
+
+
+        $newSession = new Session();
+        $form = $this->createForm(SessionFormType::class, $newSession);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $newSession = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newSession);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('session_index');
+        }
+
+
+        return $this->render('session/formSession.html.twig', [
+        'session_form' => $form->createView()
+        ]);
+     }
 
     /**
      * @Route("/{id}", name="show_one_session", methods="GET")
